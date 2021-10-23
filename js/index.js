a28a43c8e03ace168be2fbfcfd6486c43aa2a721
stritsel <84728216+stritsel@users.noreply.github.com>
2021-10-23 14:51:07 -0400

    upload HW19

---
+++ js/index.js
@@ -0,0 +1,13 @@
+// event listener: Hide a div
+  $('#click-work-title1').on("click", function(){
+    $('.work-project-blue').slideToggle();
+  });
+
+  $('#click-work-title2').on("click", function(){
+    $('.work-project-green').slideToggle();
+  });
+
+  $('#click-work-title3').on("click", function(){
+    $('.work-project-pink').slideToggle();
+  });
+
